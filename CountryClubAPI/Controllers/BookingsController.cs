8fa86edeacf22490b32ffda71d1d5e42ffea5f4e
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-10-09 13:33:02 -0600

    added controller

---
+++ CountryClubAPI/Controllers/BookingsController.cs
@@ -0,0 +1,23 @@
+﻿using CountryClubAPI.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CountryClubAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+        private readonly CountryClubContext _context;
+
+        public BookingsController(CountryClubContext context)
+        {
+            _context = context;
+        }
+
+
+
+
+
+    }
+}
